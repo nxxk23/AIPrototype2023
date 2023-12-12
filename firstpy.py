f39be4816e8e28b49844ea803b2f201b2b817e6a
nxxk23 <narakorn.v@kkumail.com>
2023-12-12 11:32:48 +0700

    homework add XX

--- firstpy.py
+++ firstpy.py
@@ -1,44 +1,38 @@
 import argparse
-from ast import parse
 
 def parse_input():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         '--num', 
-        type = int,
+        type=int,
         required=True,
         help='input for the multiplyby9 function'
     )
-    args = parser.parse_args()
-    return args
-
-def parse_input():
-    parser = argparse.ArgumentParser()
 
     parser.add_argument(
         '--XX', 
-        type = int,
+        type=int,
         default=7,
         help='input for XX'
     )
+
     args = parser.parse_args()
     return args
 
 def printHello():
-    print("Hello World!") #no input/output have only process
+    print("Hello World!")  # no input/output have only process
 
 def multiplyby9(inputV):
-    print(9*inputV) # have input and process
+    print(9 * inputV)  # have input and process
 
-if __name__=="__main__":
-
+if __name__ == "__main__":
     input_v = parse_input()
     print(f'the input num is {input_v.num}')
 
     input_v = parse_input()
     print(f'the input XX is {input_v.XX}')
 
-    print("we are in the main function") #control program's flow
+    print("we are in the main function")  # control program's flow
     multiplyby9(input_v.num)
     printHello()
