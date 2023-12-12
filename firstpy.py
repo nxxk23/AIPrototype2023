fa85f32f75056529ceb79df510b0e1007e8367b7
nxxk23 <narakorn.v@kkumail.com>
2023-12-12 10:20:52 +0700

    add function

--- firstpy.py
+++ firstpy.py
@@ -1,5 +1,10 @@
-#def main():
-#    print("Hello World!")
+def printHello():
+    print("Hello World!") #no input/output have only process
+
+def multiplyby9(inputV):
+    print(9*inputV) # have input and process
 
 if __name__=="__main__":
-    print("we are in the main function")
+    print("we are in the main function") #control program's flow
+    multiplyby9(20)
+    printHello()
