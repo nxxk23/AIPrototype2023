6d52924adcb267db57c7dab1de9fbb5a0ec29783
nxxk23 <narakorn.v@kkumail.com>
2023-12-25 16:00:25 +0700

    test

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,5 +2,11 @@ import subprocess # for execute terminal command
 
 if __name__ == "__main__":
     #basic terminal command
-    subprocess.run(["ls","-ltr"]) #look on file
-    subprocess.run(["rm","-r","/home/thisisninkspaces/testfolder1"]) # remove file
+    #subprocess.run(["ls","-ltr"]) #look on file
+    #subprocess.run(["rm","-r","/home/thisisninkspaces/testfolder1"]) # remove file
+    
+    subprocess.run(["python", "firstpy.py", "--num 100", "--XX 90"])
+    subprocess.run(["python", "firstpy.py", "--num 10", "--XX -90"])
+    subprocess.run(["python", "firstpy.py","--num 0"])
+
+    
