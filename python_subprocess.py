e43cf939518fe3063e97d9f6a1830f3087b8c991
nxxk23 <narakorn.v@kkumail.com>
2023-12-25 15:12:35 +0700

    add delete folder

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,5 +3,4 @@ import subprocess # for execute terminal command
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"]) #look on file
-    subprocess.run(["rm","-r","~/tesfolder1"]) # remove file
-    subprocess.run(["cd"])
+    subprocess.run(["rm","-r","/home/thisisninkspaces/testfolder1"]) # remove file
