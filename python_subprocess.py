11742bacb80da66383643cebc5c7e9744212f3ff
nxxk23 <narakorn.v@kkumail.com>
2023-12-25 16:14:34 +0700

    hw1

--- python_subprocess.py
+++ python_subprocess.py
@@ -4,9 +4,23 @@ if __name__ == "__main__":
     #basic terminal command
     #subprocess.run(["ls","-ltr"]) #look on file
     #subprocess.run(["rm","-r","/home/thisisninkspaces/testfolder1"]) # remove file
-    
-    subprocess.run(["python", "firstpy.py", "--num 100", "--XX 90"])
-    subprocess.run(["python", "firstpy.py", "--num 10", "--XX -90"])
-    subprocess.run(["python", "firstpy.py","--num 0"])
+    print(f"first run num=100 XX=90")
+    subprocess.run(["python", "firstpy.py", "--num", "100", "--XX", "90"])
+    print(f"------------------------------------------------------------")
+    print(f"first run num=-10 XX=-90")
+    subprocess.run(["python", "firstpy.py", "--num" ,"-10", "--XX", "-90"])
+    print(f"------------------------------------------------------------")
+    print(f"first run num=0")
+    subprocess.run(["python", "firstpy.py", "--num", "0"])
+    print(f"------------------------------------------------------------")
 
     
+    #use output from other program
+    process_output = subprocess.Popen(["python","firstpy.py","--num","0"], #ดึง output ออกมาทำงานต่อแต่ต้อง decode ก่อน
+                                      stdout = subprocess.PIPE,
+                                      stderr = subprocess.PIPE)
+    out, err = process_output.communicate()
+    print(out.decode('utf-8'))
+    print(len(out.decode('utf-8')))
+
+    ##HW เขียน subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello World!)
