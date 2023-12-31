1da6ff71472790efbea47aeb8bb634eb5fc22ca4
nxxk23 <narakorn.v@kkumail.com>
2023-12-31 14:53:13 +0700

    test

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,49 +1,30 @@
 ##HW เขียน subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello World!)
 import subprocess
-import re
-
-def run_firstpy(num, XX):
-    print(f"Running firstpy.py with num={num} XX={XX}")
-    result = subprocess.run(["python", "firstpy.py", "--num", str(num), "--XX", str(XX)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    # Extract numeric part using regular expression
-    numeric_part = re.search(r'\b\d+\b', result.stdout.decode('utf-8').strip())
-    
-    if numeric_part:
-        return int(numeric_part.group())
-    else:
-        return 0  # Return 0 if no numeric part is found
 
 if __name__ == "__main__":
     # Basic terminal command
-    #subprocess.run(["ls", "-ltr"])  # Look on file
-    #subprocess.run(["rm", "-r", "/home/thisisninkspaces/testfolder1"])  # Remove file
+    #subprocess.run(["ls", "-ltr"])  # look on file
+    #subprocess.run(["rm", "-r", "/home/thisisninkspaces/testfolder1"])  # remove file
 
-    # Store output from each run
-    outputs = []
-
+    # Capture the output of each subprocess.run command
     print(f"first run num=100 XX=90")
-    outputs.append(run_firstpy(100, 90))
-
+    output1 = subprocess.run(["python", "firstpy.py", "--num", "100", "--XX", "90"], capture_output=True)
     print(f"------------------------------------------------------------")
-    print(f"first run num=-10 XX=-90")
-    outputs.append(run_firstpy(-10, -90))
-
+    print(f"second run num=-10 XX=-90")
+    output2 = subprocess.run(["python", "firstpy.py", "--num", "-10", "--XX", "-90"], capture_output=True)
     print(f"------------------------------------------------------------")
-    print(f"first run num=0")
-    outputs.append(run_firstpy(0, 0))
-
+    print(f"third run num=0")
+    output3 = subprocess.run(["python", "firstpy.py", "--num", "0"], capture_output=True)
     print(f"------------------------------------------------------------")
 
-    # Use output from other program
-    process_output = subprocess.Popen(["python", "firstpy.py", "--num", "0"],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
-    out, err = process_output.communicate()
-    outputs.append(int(out.decode('utf-8').strip()))  # Convert output to int
+    # Print the captured output for reference
+    print("Output 1:", output1.stdout.decode('utf-8'))
+    print("Output 2:", output2.stdout.decode('utf-8'))
+    print("Output 3:", output3.stdout.decode('utf-8'))
 
-    # Summation of the numeric values from the outputs
-    total_sum = sum(outputs)
-    print(f"Total sum of all outputs: {total_sum}")
+    # Summation of the results
+    result_sum = sum(map(int, [output1.stdout.decode('utf-8'), output2.stdout.decode('utf-8'), output3.stdout.decode('utf-8')]))
+
+    print(f"Summation of results: {result_sum}")
 
 
