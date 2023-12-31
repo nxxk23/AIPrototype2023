54cf00f72017aaaf64045a100586bcfd9f5a2f68
nxxk23 <narakorn.v@kkumail.com>
2023-12-31 15:21:37 +0700

    HW1

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,5 +1,5 @@
+##HW เขียน subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello world!)
 import subprocess
-import argparse
 
 def extract_numeric(text):
     for line in text.splitlines():
@@ -7,23 +7,29 @@ def extract_numeric(text):
             return int(line.strip())
     return 0
 
-def run_command(args):
-    command = ["python", "firstpy.py", "--num", str(args.num), "--XX", str(args.XX)]
-    output = subprocess.run(command, capture_output=True)
-    result = extract_numeric(output.stdout.decode('utf-8'))
-    print(f"Output for command {command}: {result}")
-    return result
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run subprocesses and sum numeric output.")
-    
-    for i in range(1, 4):  # Assuming you want to handle 3 subprocesses, adjust the range accordingly
-        parser.add_argument(f"--num{i}", type=int, default=0, help=f"Input for subprocess {i}")
-        parser.add_argument(f"--XX{i}", type=int, default=0, help=f"Input for subprocess {i}")
+    # Basic terminal command
+    # subprocess.run(["ls", "-ltr"])  # look on file
+    # subprocess.run(["rm", "-r", "/home/thisisninkspaces/testfolder1"])  # remove file
 
-    args = parser.parse_args()
+    # Capture the output of each subprocess.run command
+    output1 = subprocess.run(["python", "firstpy.py", "--num", "100", "--XX", "90"], capture_output=True)
+    output2 = subprocess.run(["python", "firstpy.py", "--num", "-10", "--XX", "-90"], capture_output=True)
+    output3 = subprocess.run(["python", "firstpy.py", "--num", "0"], capture_output=True)
 
-    outputs = [run_command(args) for i in range(1, 4)]  # Assuming you want to handle 3 subprocesses
-    result_sum = sum(outputs)
-    
-    print(f"Summation of results: {result_sum}")
+    # Print the captured output for reference
+    print("Output 1:", output1.stdout.decode('utf-8'))
+    print("Output 2:", output2.stdout.decode('utf-8'))
+    print("Output 3:", output3.stdout.decode('utf-8'))
+
+    # Extract numeric parts, convert to integers, and print the results
+    result1 = extract_numeric(output1.stdout.decode('utf-8'))
+    result2 = extract_numeric(output2.stdout.decode('utf-8'))
+    result3 = extract_numeric(output3.stdout.decode('utf-8'))
+
+    print(f"Result 1: {result1}")
+    print(f"Result 2: {result2}")
+    print(f"Result 3: {result3}")
+
+    # Sum the results
+    result_sum = sum([result1, result2, result3])
