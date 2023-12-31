23f1e8b0195ad6c787d6a4de2f3267112b82791b
nxxk23 <narakorn.v@kkumail.com>
2023-12-31 14:45:33 +0700

    test

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,38 +1,41 @@
 ##HW เขียน subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello World!)
 import subprocess
 
-def run_command(command):
-    process = subprocess.Popen(command,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               text=True)
-    out, err = process.communicate()
-    return out.strip()
+def run_firstpy(num, XX):
+    print(f"Running firstpy.py with num={num} XX={XX}")
+    result = subprocess.run(["python", "firstpy.py", "--num", str(num), "--XX", str(XX)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return int(result.stdout.decode('utf-8').strip())  # Convert output to int
 
 if __name__ == "__main__":
     # Basic terminal command
-    subprocess.run(["ls", "-ltr"])  # look on file
-    subprocess.run(["rm", "-r", "/home/thisisninkspaces/testfolder1"])  # remove file
+    #subprocess.run(["ls", "-ltr"])  # Look on file
+    #subprocess.run(["rm", "-r", "/home/thisisninkspaces/testfolder1"])  # Remove file
 
-    # Run firstpy.py with different inputs
-    commands = [
-        ["python", "firstpy.py", "--num", "100", "--XX", "90"],
-        ["python", "firstpy.py", "--num", "-10", "--XX", "-90"],
-        ["python", "firstpy.py", "--num", "0"]
-    ]
+    # Store output from each run
+    outputs = []
 
-    results = []
-
-    for command in commands:
-        result = run_command(command)
-        try:
-            result_int = int(result)
-            results.append(result_int)
-        except ValueError:
-            print(f"Non-integer output: {result}")
-
-    # Summation of results
-    total_sum = sum(results)
+    print(f"first run num=100 XX=90")
+    outputs.append(run_firstpy(100, 90))
 
     print(f"------------------------------------------------------------")
-    print(f"Summation of results: {total_sum}")
+    print(f"first run num=-10 XX=-90")
+    outputs.append(run_firstpy(-10, -90))
+
+    print(f"------------------------------------------------------------")
+    print(f"first run num=0")
+    outputs.append(run_firstpy(0, 0))
+
+    print(f"------------------------------------------------------------")
+
+    # Use output from other program
+    process_output = subprocess.Popen(["python", "firstpy.py", "--num", "0"],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+    out, err = process_output.communicate()
+    outputs.append(int(out.decode('utf-8').strip()))  # Convert output to int
+
+    # Summation of the numeric values from the outputs
+    total_sum = sum(outputs)
+    print(f"Total sum of all outputs: {total_sum}")
+
+
