cdc50cb8b3de3c93dbfcc778ce7876708f5da27a
nxxk23 <narakorn.v@kkumail.com>
2024-01-15 15:43:52 +0700

    test

--- firstflask.py
+++ firstflask.py
@@ -8,5 +8,9 @@ app = Flask(__name__) #start server
 def helloworld():
     return "Hello, World!"
 
+@app.route("/") #add route for search web
+def hellonink():
+    return "Hello, Nink!"
+
 if __name__=="__main__": 
     app.run(host='0.0.0.0', debug=True,port=5001) #host=0.0.0.0 :)
