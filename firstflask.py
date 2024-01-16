33d98c4a076a67d082cd9a5bbd5d1385e51d794a
nxxk23 <narakorn.v@kkumail.com>
2024-01-16 09:39:39 +0700

    add input

--- firstflask.py
+++ firstflask.py
@@ -4,17 +4,23 @@ import json
 
 app = Flask(__name__) #start server
 
-@app.route("/") #add route for search web
+@app.route("/") #add route for search web1
 def helloworld():
     return "Hello, World!"
 
-@app.route("/name") #add route for search web
+@app.route("/name") #add route for search web2
 def hellonink():
     return "Hello, Nink!"
 
-@app.route("/home2") #add route for search web
-def home2():
-    return render_template("home.html",name='nink')
+@app.route("/home", methods={'POST'}) #add route for search web
+def homefn():
+    print('we are in home')
+    #getting input with name = fname in HTML form
+    namein = request.form.get('fname')
+    lastnamein = request.form.get('lname')
+    print(namein)
+    print(lastnamein) #addinput from web into our html
+    return render_template("home.html", name=namein)
 
 if __name__=="__main__": 
-    app.run(host='0.0.0.0', debug=True,port=5001) #host=0.0.0.0 :)
+    app.run(host='0.0.0.0', debug=True, port=5001) #host=0.0.0.0 :)
